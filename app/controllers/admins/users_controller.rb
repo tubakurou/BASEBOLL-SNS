353830dffa092f1c1faba9bddaa7e547e100e9c9
tubakurou <yamazawa1300@gmail.com>
2022-02-27 17:59:19 +0000

    W

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,5 +1,5 @@
 class Admins::UsersController < ApplicationController
-  before_action user_admin, only: [:index]
+  before_action :if_not_admins
 def show
   @user = User.find(params[:id])
   @game_comments = @user.game_comments.includes
@@ -33,7 +33,7 @@ private
   def user_params
     params.require(:user).permit(:team_id, :last_name, :first_name, :last_name_kana, :first_name_kana, :profile, :user_image_id, :is_deleted)
   end
-  
+
   def if_not_admin
     redirect_to root_path unless current_.admin?
   end
