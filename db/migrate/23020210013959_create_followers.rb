61afabd4601c2a95a37680882b07dcf452d57a3c
tubakurou <yamazawa1300@gmail.com>
2022-02-27 17:16:23 +0000

    7

--- db/migrate/23020210013959_create_followers.rb
+++ db/migrate/23020210013959_create_followers.rb
@@ -1,8 +1,8 @@
 class CreateFollowers < ActiveRecord::Migration[5.2]
   def change
     create_table :followers do |t|
-      #t.references :follower, foreign_key: true, null: false
-      #t.references :followerd, foreign_key: true, null: false
+      t.references :follower, foreign_key: true, null: false
+      t.references :followerd, foreign_key: true, null: false
 
       t.timestamps
     end
